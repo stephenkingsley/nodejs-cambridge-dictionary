0a8b7b85ec718759127bb1eb60456c5576265853
stephenkingsley <stephenkingsley@foxmail.com>
2020-04-11 01:33:49 +0800

    fix: linux sandbox

--- app/service/vocabularies.js
+++ app/service/vocabularies.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 
 class VocabulariesService extends Service {
   async get(searchWord) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X)');
     await page.setViewport({width:375, height:812});
